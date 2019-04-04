99fafb38bdd127e76458b4b7441a3076a1fa79e7
SaloniGahlaut <45948258+SaloniGahlaut@users.noreply.github.com>
2019-04-04 13:45:57 +0530

    Update shelldev.sh

--- shelldev.sh
+++ shelldev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws lambda publish-version --function-name generatecsvfromxls-ms --region us-east-1 --output=text --query Version
-version=$(aws lambda publish-version --function-name generatecsvfromxls-ms --output=text --query  'Version') && aws lambda update-alias --function-name generatecsvfromxls-ms --name Dev --function-version $version
+aws lambda publish-version --function-name My_Lambda --region us-east-1 --output=text --query Version
+version=$(aws lambda publish-version --function-name My_Lambda --output=text --query  'Version') && aws lambda update-alias --function-name My_Lambda --name Dev --function-version $version
